1a2aa80037e40509bc8d95eb3514910f51bd55a7
Harold Flores <41088459+Slicez@users.noreply.github.com>
2021-06-30 17:21:46 -0500

    update
    added name

--- guessing_game.py
+++ guessing_game.py
@@ -8,36 +8,36 @@ Project 1 - Number Guessing Game
 import random
 # print a welcome screen 
 print("Welcome to Harold Number gussing game")
-username = input("Please enter your name?: ")
-print("Welcome {} Lets's begin!".format(username))
+name = input("What is your name?: ")
+print(name)
 
 def start_game():
     attempts = 1
-    random_numbers = random.randint(1,10)
-    users_try = 0
-    while users_try != random_numbers :
+    guess = random.randint(1,10)
+    tries = 0
+    while tries != guess:
         try:
-             users_try = int(input("Pick a number between 1 and 10: "))
-             if users_try < 1 or users_try > 10:
-                print("Hold up your number must be between 1 and 10. Try again!")
+            tries = int(input("Pick a number between 1 and 10: "))
+            if tries < 1 or tries > 10:
+                print("The number must be between 1 and 10 please try again!")
                 continue
         except ValueError:
-                print("Hold up the number must be an interger between 1 and 10. Try again!")
+            print("The number must be an interger between 1 and 10. Try again")
         else:
-            if users_try > random_numbers:
+            
+            if tries > guess:
                 attempts += 1
-                print("The number you type is to high please try again: ")
-            elif users_try < random_numbers:
+                print("Your guess is to high! Try again: ")
+            elif tries < guess:
                 attempts += 1
-                print("The number you type is to low please try again: ")
-            else:
-                print("You got the right number The number was {} and it took you {} tries. Good Job".format(username,random_numbers,attempts))
-            return attempts
-
-    
+                print("Your guess is to low! Try again: ")
+    else:
+         print("You got the right number {}! the number was {} and it took you {} attempts. Congrats🎉!".format(name,Haguess,attempts))
+    return attempts
 
 
 
 # Kick off the program by calling the start_game function.
 start_game()
+
 
