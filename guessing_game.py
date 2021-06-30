9f7469818189c6a54970797de6f593f78300de1e
Harold Flores <41088459+Slicez@users.noreply.github.com>
2021-06-30 16:16:49 -0500

    test 1
    add guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,41 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+"""
+
+
+import random
+# print a welcome screen 
+name = input("What is your name?:   ")
+print("Welcome {} to Harold Number gussing game".format(name))
+
+
+def start_game():
+    attempts = 1
+    random_numbers = random.randint(1,10)
+    users_try = 0
+    while users_try != random_numbers :
+        try:
+             users_try = int(input("Pick a number between 1 and 10: "))
+             if users_try < 1 or users_try > 10:
+                print("Hold up your number must be between 1 and 10. Try again!")
+                continue
+        except ValueError:
+                print("Hold up the number must be an interger between 1 and 10. Try again!")
+        else:
+            if users_try > random_numbers:
+                attempts += 1
+                print("The number you type is to high please try again: ")
+            elif users_try < random_numbers:
+                attempts += 1
+                print("The number you type is to low please try again: ")
+            else:
+                print("You got the right number {}! The number was {} and it took you {} tries. Good Job".format(name,random_numbers,attempts))
+            return attempts
+    
+
+
+
+# Kick off the program by calling the start_game function.
+start_game()
